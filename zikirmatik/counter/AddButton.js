940c41ab4313f817509ce3110e8c7aef0f89b17b
elifdenizEsea <ce.elif@gmail.com>
2024-04-26 13:23:55 +0100

    Styles fixed

--- zikirmatik/counter/AddButton.js
+++ zikirmatik/counter/AddButton.js
@@ -2,10 +2,10 @@ import { Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
 import CustomStyle from '../style/customStyle';
 
-const AddButton = ({onAddPress},{labelOfButton}) => {
+const AddButton = ({onAddPress,labelOfButton}) => {
   return (
     <TouchableOpacity style={CustomStyle.countButton} onPress={onAddPress}> 
-    <Text>{labelOfButton}</Text>
+    <Text style={CustomStyle.countText} >{labelOfButton}</Text>
     
     </TouchableOpacity>
   );
