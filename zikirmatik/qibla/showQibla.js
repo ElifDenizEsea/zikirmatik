68094b12648fa71ffb8214fef82824ab44db656a
elifdenizEsea <ce.elif@gmail.com>
2024-05-07 22:51:09 +0100

    qibla button added

---
+++ zikirmatik/qibla/showQibla.js
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+const showQibla = ({navigation}) => {
+    return (
+       
+        <Text>dfdf </Text>
+        
+      );
+    }
+export default showQibla;
