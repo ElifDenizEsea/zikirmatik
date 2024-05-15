6ce807352eaf1e206be61b36ad6720c8dd62e025
elifdenizEsea <ce.elif@gmail.com>
2024-05-15 01:00:54 +0100

    saveto file

--- zikirmatik/list/AddToList.js
+++ zikirmatik/list/AddToList.js
@@ -1,32 +1,53 @@
-import React  from 'react';
+import React, { useState } from 'react';
 import {View, Text,TextInput,Pressable} from 'react-native';
 import CustomStyle from '../style/customStyle';
 import {useTranslation} from 'react-i18next';
 import i18n from "../text/general";
-import DATA from '../files/list.json';
-
+import * as FileSystem from "expo-file-system"
 
 const initI18n = i18n;
 
-const save=()=>{
-  
-}
-
 
 const AddToList = ({navigation}) => {
+
+const [zikrName, onChangeZikrName] = React.useState('');
+const [zikrRead, onChangeZikrRead] = React.useState('');
+const [zikrNumber, onChangeZikrNumber] = React.useState('');
+
+  const saveFile = async () => {
+    const newZikr={
+      name:zikrName,
+      howToRead:zikrRead,
+      number:zikrNumber,
+      lastCount:0
+    };
+  
+    let filename = FileSystem.documentDirectory+"list.json";
+    const aa=await FileSystem.readAsStringAsync(filename);
+    var jsonArray = [];
+    jsonArray=JSON.parse(aa);
+  console.log(jsonArray); 
+  jsonArray.push(newZikr);
+    await FileSystem.writeAsStringAsync(filename, JSON.stringify(jsonArray), { encoding: FileSystem.EncodingType.UTF8 });
+    console.log("3");
+  };
+  const aa=FileSystem.readAsStringAsync(FileSystem.documentDirectory+"text.txt");
 
   const {t, i18n} = useTranslation();
   return (
   <View style={{ flex: 1,}}>
     <Text  style={CustomStyle.resetButton} >{t('ADD_TO_LIST.ZIKR_NAME')} </Text>
-    <TextInput  style={CustomStyle.inputNumber} />
+    <TextInput               onChangeText={onChangeZikrName}
+ style={CustomStyle.inputNumber} />
 
     <Text  style={CustomStyle.resetButton} >{t('ADD_TO_LIST.HOW_TO_READ')}</Text>
-    <TextInput   style={CustomStyle.inputNumber}   />
+    <TextInput                onChangeText={onChangeZikrRead}
+ style={CustomStyle.inputNumber}   />
 
     <Text  style={CustomStyle.resetButton} >{t('ADD_TO_LIST.INPUT_ZIKR')}</Text>
-    <TextInput  style={CustomStyle.inputNumber}  keyboardType="numeric"  />
-    <Pressable style={CustomStyle.listButton}  onPress={save}>
+    <TextInput               onChangeText={onChangeZikrNumber}
+  style={CustomStyle.inputNumber}  keyboardType="numeric"  />
+    <Pressable style={CustomStyle.listButton}  onPress={saveFile}>
       <Text>save</Text>
     </Pressable>
   </View>
